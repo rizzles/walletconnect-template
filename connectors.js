899522897a83e24ebf8f7467b75920ca9473e160
Mike Reilly <reillysemail@gmail.com>
2021-02-03 16:33:08 -0800

    added injected connector

--- connectors.js
+++ connectors.js
@@ -1,14 +1,19 @@
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { InjectedConnector } from '@web3-react/injected-connector';
 
-const POLLING_INTERVAL = 12000
+const POLLING_INTERVAL = 12000;
 const RPC_URLS = {
-  1: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213',
-  4: 'https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213'
-}
+    1: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213',
+    4: 'https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213',
+};
+
+export const injected = new InjectedConnector({
+    supportedChainIds: [1, 3, 4, 5, 42],
+});
 
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: RPC_URLS[1] },
-  bridge: 'https://bridge.walletconnect.org',
-  qrcode: true,
-  pollingInterval: POLLING_INTERVAL
-})
+    rpc: { 1: RPC_URLS[1] },
+    bridge: 'https://bridge.walletconnect.org',
+    qrcode: true,
+    pollingInterval: POLLING_INTERVAL,
+});
--- pages/index.js
+++ pages/index.js
@@ -1,119 +1,137 @@
-import { useState, useEffect } from 'react';
-import { Web3ReactProvider, useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
+import React, { useState, useEffect } from 'react';
+import {
+    Web3ReactProvider,
+    useWeb3React,
+    UnsupportedChainIdError,
+} from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import { formatEther } from '@ethersproject/units';
 import { walletconnect } from '../connectors';
-import { Web3Provider } from '@ethersproject/providers'
-import { formatEther } from '@ethersproject/units'
 import { Spinner } from '../components/Spinner';
 
 function getLibrary(provider) {
-  const library = new Web3Provider(provider)
-  library.pollingInterval = 12000
-  return library
+    const library = new Web3Provider(provider);
+    library.pollingInterval = 12000;
+    return library;
 }
 
-export default function() {
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <App />
-    </Web3ReactProvider>
-  )
+export default function () {
+    return (
+        <Web3ReactProvider getLibrary={getLibrary}>
+            <App />
+        </Web3ReactProvider>
+    );
 }
 
 function Balance() {
-  const { account, library, chainId } = useWeb3React()
+    const { account, library, chainId } = useWeb3React();
 
-  const [balance, setBalance] = useState()
-  useEffect(() => {
-    if (!!account && !!library) {
-      let stale = false
+    const [balance, setBalance] = useState();
+    useEffect(() => {
+        if (!!account && !!library) {
+            let stale = false;
 
-      library
-        .getBalance(account)
-        .then((balance) => {
-          if (!stale) {
-            setBalance(balance)
-          }
-        })
-        .catch(() => {
-          if (!stale) {
-            setBalance(null)
-          }
-        })
+            library
+                .getBalance(account)
+                .then((balance) => {
+                    if (!stale) {
+                        setBalance(balance);
+                    }
+                })
+                .catch(() => {
+                    if (!stale) {
+                        setBalance(null);
+                    }
+                });
 
-      return () => {
-        stale = true
-        setBalance(undefined)
-      }
-    }
-  }, [account, library, chainId]) // ensures refresh if referential identity of library doesn't change across chainIds
+            return () => {
+                stale = true;
+                setBalance(undefined);
+            };
+        }
+    }, [account, library, chainId]);
 
-  return (
-    <>
-      <span>Balance</span>
-      <span role="img" aria-label="gold">
-        💰
-      </span>
-      <span>{balance === null ? 'Error' : balance ? `Ξ${formatEther(balance)}` : ''}</span>
-    </>
-  )
+    return (
+        <>
+            <span>Balance</span>
+            <span role="img" aria-label="gold">
+                💰
+            </span>
+            <span>
+                {balance === null
+                    ? 'Error'
+                    : balance
+                    ? `Ξ${formatEther(balance)}`
+                    : ''}
+            </span>
+        </>
+    );
 }
 
 const App = () => {
-  const context = useWeb3React();
-  const { connector, library, chainId, account, activate, deactivate, active, error } = context;
-  const [activatingConnector, setActivatingConnector] = useState();
+    const context = useWeb3React();
+    const {
+        connector,
+        library,
+        chainId,
+        account,
+        activate,
+        deactivate,
+        active,
+        error,
+    } = context;
+    const [activatingConnector, setActivatingConnector] = useState();
 
-  useEffect(() => {
-    if (activatingConnector && activatingConnector === connector) {
-      setActivatingConnector(undefined)
-    }
-  }, [activatingConnector, connector])
+    useEffect(() => {
+        if (activatingConnector && activatingConnector === connector) {
+            setActivatingConnector(undefined);
+        }
+    }, [activatingConnector, connector]);
 
-  const currentConnector = walletconnect;
-  const activating = currentConnector === activatingConnector;
-  const connected = currentConnector === connector;
-  const disabled = !!activatingConnector || connected || !!error
-  const name = 'WalletConnect';
+    const currentConnector = walletconnect;
+    const activating = currentConnector === activatingConnector;
+    const connected = currentConnector === connector;
+    const disabled = !!activatingConnector || connected || !!error;
+    const name = 'WalletConnect';
 
-  return (
-    <>
-      <button
-        style={{
-          height: '3rem',
-          borderRadius: '1rem',
-          borderColor: activating ? 'orange' : connected ? 'green' : 'unset',
-          cursor: disabled ? 'unset' : 'pointer',
-          position: 'relative'
-        }}
-        disabled={disabled}
-        key={name}
-        onClick={() => {
-          setActivatingConnector(currentConnector)
-          activate(walletconnect)
-        }}
-      >
-        <div
-          style={{
-            position: 'absolute',
-            top: '0',
-            left: '0',
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            color: 'black',
-            margin: '0 0 0 1rem'
-          }}
-        >
-          {activating && <Spinner color={'black'} style={{ height: '25%', marginLeft: '-1rem' }} />}
-          {connected && (
-            <span role="img" aria-label="check">
-              ✅
-            </span>
-          )}
-        </div>
-        {name}
-      </button>
-      <Balance />
-    </>
-  )
-}
+    return (
+        <>
+            <button
+                disabled={disabled}
+                key={name}
+                onClick={() => {
+                    setActivatingConnector(currentConnector);
+                    activate(walletconnect);
+                }}
+                type="button"
+            >
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: '0',
+                        left: '0',
+                        height: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                        color: 'black',
+                        margin: '0 0 0 1rem',
+                    }}
+                >
+                    {activating && (
+                        <Spinner
+                            color="black"
+                            style={{ height: '25%', marginLeft: '-1rem' }}
+                        />
+                    )}
+                    {connected && (
+                        <span role="img" aria-label="check">
+                            ✅
+                        </span>
+                    )}
+                </div>
+                {name}
+            </button>
+            <Balance />
+        </>
+    );
+};
